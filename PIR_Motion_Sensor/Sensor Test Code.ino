7b52c507c03b92a7a51205c1f10409c4c8cfe371
Sam Schimanski <108167199+Sam-Schimanski@users.noreply.github.com>
2022-06-27 13:30:32 +1200

    Update Sensor Test Code.ino
    Formatting.

--- PIR_Motion_Sensor/Sensor Test Code.ino
+++ PIR_Motion_Sensor/Sensor Test Code.ino
@@ -1,15 +1,15 @@
 #define PIRPIN 7
 
 void setup() {
-  pinMode(PIRPIN,INPUT); //set PIRPIN as input
-  pinMode(LED_BUILTIN, OUTPUT); //Set built in LED as output
+    pinMode(PIRPIN,INPUT); //set PIRPIN as input
+    pinMode(LED_BUILTIN, OUTPUT); //Set built in LED as output
 }
 
 void loop() {
-  bool pir; //Variable to hold input state
-  pir = digitalRead(PIRPIN); //Read input
-  digitalWrite(LED_BUILTIN ,pir); //Set built in led to match
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW); //Reset built in LED to low
-  Serial.println(pir); //Print pir output to monitor
+    bool pir; //Variable to hold input state
+    pir = digitalRead(PIRPIN); //Read input
+    digitalWrite(LED_BUILTIN ,pir); //Set built in led to match
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW); //Reset built in LED to low
+    Serial.println(pir); //Print pir output to monitor
 }
